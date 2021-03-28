5686498aace7cbac444a1cd7a3fdb5af60bb0a0b
Rafaël Naciri <rafael.naciri.dev@gmail.com>
2021-03-28 03:46:13 +0200

    fix: removed String message

---
+++ Makefile
@@ -0,0 +1,4 @@
+genapi:
+	protoc --proto_path=grpc -I=/usr/include -I=protos --go_out=pkg/gen --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative --dart_out=grpc:lib grpc/*.proto protos/*.proto
+gengoogle:
+	protoc -I/usr/include --dart_out=grpc:lib /usr/include/google/protobuf/*.proto
--- lib/connector.pbjson.dart
+++ lib/connector.pbjson.dart
@@ -8,13 +8,3 @@
 import 'dart:core' as $core;
 import 'dart:convert' as $convert;
 import 'dart:typed_data' as $typed_data;
-@$core.Deprecated('Use stringDescriptor instead')
-const String$json = const {
-  '1': 'String',
-  '2': const [
-    const {'1': 'string', '3': 1, '4': 1, '5': 9, '10': 'string'},
-  ],
-};
-
-/// Descriptor for `String`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List stringDescriptor = $convert.base64Decode('CgZTdHJpbmcSFgoGc3RyaW5nGAEgASgJUgZzdHJpbmc=');
